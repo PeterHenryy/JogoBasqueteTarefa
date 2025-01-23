f3955a78c6a0c0d9a245a4d740426476a436ea48
PeterHenryy <snakenoxz@gmail.com>
2025-01-23 18:06:33 -0300

    modelo jogo e libs

---
+++ Models/Jogo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JogoBasqueteTarefa.Models
+{
+    public class Jogo
+    {
+        [Key]
+        public int ID { get; set; }
+        public DateTime Data { get; set; }
+        public int Pontos { get; set; }
+    }
+}
